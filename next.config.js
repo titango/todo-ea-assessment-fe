0286dbeb35b5e3cbd96b62ed24f0630c3f46bc3d
Tan Thinh Ngo <ngotanthinh92@gmail.com>
2023-05-16 15:14:59 -0700

    add env build

--- next.config.js
+++ next.config.js
@@ -1,11 +1,23 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
 
+const Dotenv = require("dotenv-webpack")
+
 const nextConfig = {
   reactStrictMode: false,
   output: 'standalone',
   sassOptions: {
     includePaths: [path.join(__dirname, 'src/styles')],
+  },
+  webpack: config => {
+    config.plugins.push(
+      new Dotenv({
+        path: `./.env.${
+          process.env.NODE_ENV === "production" ? "production" : "development"
+        }`,
+      })
+    )
+    return config
   },
 }
 
