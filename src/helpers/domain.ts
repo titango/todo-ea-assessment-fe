08bca579363ac48e82b79a2705852a982968c362
Tan Thinh Ngo <ngotanthinh92@gmail.com>
2023-05-16 16:50:31 -0700

    update prod vars

--- src/helpers/domain.ts
+++ src/helpers/domain.ts
@@ -2,5 +2,5 @@ export function backendDomain() {
   const schema = process.env.NEXT_PUBLIC_BACKEND_SCHEME;
   const host = process.env.NEXT_PUBLIC_BACKEND_HOST;
   const port = process.env.NEXT_PUBLIC_BACKEND_PORT;
-  return `${schema}://${host}:${port}`;
+  return `${schema}://${host}${port ? `:${port}` : ""}`;
 }
