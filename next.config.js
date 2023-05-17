5492011b77755faadea5ae13c70c19a62ba02122
Tan Thinh Ngo <ngotanthinh92@gmail.com>
2023-05-16 17:15:19 -0700

    attempt fix netlify

--- next.config.js
+++ next.config.js
@@ -6,6 +6,20 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'src/styles')],
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://lighthearted-quokka-e866ac.netlify.app" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
 }
 
 module.exports = nextConfig
